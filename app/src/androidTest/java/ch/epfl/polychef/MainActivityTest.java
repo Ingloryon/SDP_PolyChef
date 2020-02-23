8d80f577ee4fb4a45b1bd22743b4d127616aaabc
Guillaume Parchet <guillaume.parchet@epfl.ch>
2020-02-23 14:27:42 +0100

    Fix button name (string import) and more alphabetical imports order

--- app/src/androidTest/java/ch/epfl/polychef/MainActivityTest.java
+++ app/src/androidTest/java/ch/epfl/polychef/MainActivityTest.java
@@ -3,8 +3,8 @@ package ch.epfl.polychef;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.typeText;
-import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
@@ -20,6 +20,7 @@ public class MainActivityTest {
     @Rule
     public final ActivityTestRule<MainActivity> mActivityRule =
             new ActivityTestRule<>(MainActivity.class);
+
     @Test
     public void testCanGreetUsers() {
         onView(withId(R.id.mainName)).perform(typeText("from my unit test")).perform(closeSoftKeyboard());
