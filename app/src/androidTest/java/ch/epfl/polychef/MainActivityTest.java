52d1692bda45ba5ff679ad38726845565ad34c94
Guillaume Parchet <guillaume.parchet@epfl.ch>
2020-02-23 14:40:12 +0100

    Sets imports in alphabetical order

--- app/src/androidTest/java/ch/epfl/polychef/MainActivityTest.java
+++ app/src/androidTest/java/ch/epfl/polychef/MainActivityTest.java
@@ -1,9 +1,9 @@
 package ch.epfl.polychef;
 
+import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.typeText;
-import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
