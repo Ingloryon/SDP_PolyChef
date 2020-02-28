77534b35b5625ca1c3c495848d47aed6212d50ca
Guillaume Parchet <guillaume.parchet@epfl.ch>
2020-02-29 00:17:01 +0100

    Create testing class for RecipeBuilder

---
+++ app/src/test/java/ch/epfl/polychef/RecipeBuilderTest.java
@@ -0,0 +1,15 @@
+package ch.epfl.polychef;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class RecipeBuilderTest {
+
+
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+
+
+}
