4793457697b36627fc85216650a0e4e755e3193f
LucasSurmely <lucas.surmely@hotmail.com>
2020-02-29 17:40:17 +0100

    added recipe name and rating to the full recipe activity

--- app/src/main/java/ch/epfl/polychef/RecipeActivity.java
+++ app/src/main/java/ch/epfl/polychef/RecipeActivity.java
@@ -1,6 +1,7 @@
 package ch.epfl.polychef;
 
 import android.os.Bundle;
+import android.widget.RatingBar;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,20 +14,32 @@ public class RecipeActivity extends AppCompatActivity {
     // Need to have a intent that have the recipe to display
     // For now I will hardcode one for example purpose
     public Recipe defaultRecipe = new RecipeBuilder()
-            .setName("Tartare de poulet")
+            .setName("Chicken tartar")
             .setRecipeDifficulty(Recipe.Difficulty.INTERMEDIATE)
             .setEstimatedCookingTime(30)
             .setPersonNumber(4)
             .setEstimatedPreparationTime(40)
-            .addIngredient("Poulet cru", 500)
+            .addIngredient("Raw chicken", 500)
             .addIngredient("Parsley", 3)
             .addInstruction("Don't cook this it's dangerous !", 0)
+            .addInstruction("Cut the chicken into small pieces", 1)
+            .addInstruction("Add salt and pepper", 2)
             .build();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fullrecipe);
+        // Recipe name
         TextView recipeName = findViewById(R.id.recipeName);
+        recipeName.setText(defaultRecipe.getName());
+        // Rating
+        final RatingBar ratingBar = findViewById(R.id.ratingBar);
+        // TODO remove me when rating is implemented in the application
+        defaultRecipe.getRating().addRate(3, 2);
+        defaultRecipe.getRating().addRate(4, 5);
+        ratingBar.setRating((float) defaultRecipe.getRating().ratingAverage());
+        // Prep time
     }
 }
