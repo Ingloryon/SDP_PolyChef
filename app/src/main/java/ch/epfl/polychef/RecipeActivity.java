256d59b56ea840fb66c471f020faaa13474c2b22
LucasSurmely <lucas.surmely@hotmail.com>
2020-03-01 18:34:03 +0100

    finished first version of full recipe activity with basic layout and activity class

--- app/src/main/java/ch/epfl/polychef/RecipeActivity.java
+++ app/src/main/java/ch/epfl/polychef/RecipeActivity.java
@@ -5,6 +5,10 @@ import android.widget.RatingBar;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+import java.util.Map;
 
 import ch.epfl.polychef.RecipeObj.Recipe;
 import ch.epfl.polychef.RecipeObj.RecipeBuilder;
@@ -31,23 +35,91 @@ public class RecipeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fullrecipe);
-        // Recipe name
+        displayRecipeName();
+        displayRating();
+        displayPrepAndCookTime();
+        displayDifficulty();
+        displayInstructions();
+        displayIngredients();
+    }
+
+    /**
+     * Display the recipe name in the correct field in the activity
+     */
+    private void displayRecipeName(){
         TextView recipeName = findViewById(R.id.recipeName);
         recipeName.setText(defaultRecipe.getName());
-        // Rating
+    }
+
+    /**
+     * Display the rating of the recipe in the correct field in the activity
+     */
+    private void displayRating(){
         final RatingBar ratingBar = findViewById(R.id.ratingBar);
         // TODO remove me when rating is implemented in the application
         defaultRecipe.getRating().addRate(3, 2);
         defaultRecipe.getRating().addRate(4, 5);
         ratingBar.setRating((float) defaultRecipe.getRating().ratingAverage());
-        // Prep time
+    }
+
+    /**
+     * Display both the preparation time and the cooking time in the correct field in the activity
+     */
+    private void displayPrepAndCookTime(){
         TextView prepTime = findViewById(R.id.prepTime);
         prepTime.setText("Prep time : "+defaultRecipe.getEstimatedPreparationTime()+" mins");
-        // Cook time
         TextView cookTime = findViewById(R.id.cookTime);
         cookTime.setText("Cook time : "+defaultRecipe.getEstimatedCookingTime()+" mins");
-        // Difficulty
-        TextView difficulty = findViewById(R.id.difficulty);
-        difficulty.setText("Difficulty : " + defaultRecipe.getRecipeDifficulty().toString());
     }
+
+    /**
+     * Display the difficulty in the correct field in the activity
+     */
+    private void displayDifficulty(){
+        TextView difficulty = findViewById(R.id.difficulty);
+        String diff = defaultRecipe.getRecipeDifficulty().toString();
+        String finalDiffStr = diff.substring(0, 1).toUpperCase().concat(diff.substring(1, diff.length()).toLowerCase().replaceAll("_", " "));
+        difficulty.setText("Difficulty : " + finalDiffStr);
+    }
+
+    /**
+     * Display all the ingredients from the list in a big string
+     */
+    private void displayIngredients(){
+        StringBuilder strBuilder = new StringBuilder();
+        String newLine = "\n";
+        Map<String, Double> allIngredients = defaultRecipe.getIngredients();
+        for(Map.Entry<String, Double> ingredient : allIngredients.entrySet()){
+            strBuilder.append("● ");
+            strBuilder.append(ingredient.getValue() + " grams of ");
+            strBuilder.append(ingredient.getKey().toLowerCase());
+            strBuilder.append(newLine);
+            strBuilder.append(newLine);
+        }
+        // Remove the last line return since there is no more ingredients to display
+        strBuilder.deleteCharAt(strBuilder.length() - 1);
+        TextView ingredients = findViewById(R.id.ingredientsList);
+        ingredients.setText(strBuilder.toString());
+    }
+
+    /**
+     * Display all the instructions from the list in a big string
+     */
+    private void displayInstructions(){
+        StringBuilder strBuilder = new StringBuilder();
+        String newLine = "\n";
+        List<String> allInstructions = defaultRecipe.getRecipeInstructions();
+        for(int i = 0; i < allInstructions.size(); i++){
+            strBuilder.append(i + 1);
+            strBuilder.append(". ");
+            strBuilder.append(allInstructions.get(i));
+            strBuilder.append(newLine);
+            strBuilder.append(newLine);
+        }
+        // Remove the last line return since there is no more instructions to display
+        strBuilder.deleteCharAt(strBuilder.length() - 1);
+        TextView instructions = findViewById(R.id.instructionsList);
+        instructions.setText(strBuilder.toString());
+    }
+
 }
