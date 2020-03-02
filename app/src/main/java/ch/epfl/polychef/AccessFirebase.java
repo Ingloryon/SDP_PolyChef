cd383c20d1664d27da547132e618eb55a498eb2b
wilderino <thomas.wilde@epfl.ch>
2020-03-02 17:06:47 +0100

    adding parameters to the two functions in AccessFirebase

--- app/src/main/java/ch/epfl/polychef/AccessFirebase.java
+++ app/src/main/java/ch/epfl/polychef/AccessFirebase.java
@@ -18,19 +18,11 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class AccessFirebase {
-    private void addDataToFirebase(){
-        final String TAG = "yo";
+    private void addDataToFirebase(Map<String, Object> collection, String path, final String TAG){
         // Access a Cloud Firestore instance from your Activity
         FirebaseFirestore db = FirebaseFirestore.getInstance();
-        // Create a new user with a first and last name
-        Map<String, Object> user = new HashMap<>();
-        user.put("first", "Ada");
-        user.put("last", "Lovelace");
-        user.put("born", 1815);
-
-
-        // Add a new document with a generated ID
-        db.collection("users").add(user).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+         // Add a new document with a generated ID
+        db.collection(path).add(collection).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
             @Override
             public void onSuccess(DocumentReference documentReference) {
                 Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
@@ -42,11 +34,10 @@ public class AccessFirebase {
             }
         });
     }
-    private void readDataFromFirebase(){
-        final String TAG = "yo";
+    private void readDataFromFirebase(String path, final String TAG){
         // Access a Cloud Firestore instance from your Activity
         FirebaseFirestore db = FirebaseFirestore.getInstance();
-        db.collection("users")
+        db.collection(path)
                 .get()
                 .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
                     @Override
