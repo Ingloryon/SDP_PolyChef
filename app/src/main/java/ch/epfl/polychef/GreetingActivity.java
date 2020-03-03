3dcf9bb28a946162c703f3b9fc0ba2ac299a7b95
wilderino <thomas.wilde@epfl.ch>
2020-03-03 17:09:49 +0100

    putting the good private key

--- app/src/main/java/ch/epfl/polychef/GreetingActivity.java
+++ app/src/main/java/ch/epfl/polychef/GreetingActivity.java
@@ -2,9 +2,23 @@ package ch.epfl.polychef;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.database.core.Tag;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.io.FileInputStream;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class GreetingActivity extends AppCompatActivity {
@@ -21,6 +35,5 @@ public class GreetingActivity extends AppCompatActivity {
         // Capture the layout's TextView and set the string as its text
         TextView textView = findViewById(R.id.greetingMessage);
         textView.setText("Hello "+message+"!");
-        
     }
 }
