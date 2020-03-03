e42271db0416629fef2b5e9caf818c60ac8fcb7a
Simon Hayoz <hayoz.simon@bluewin.ch>
2020-03-03 21:40:58 +0100

    Fix CodeClimate import order login

--- app/src/androidTest/java/ch/epfl/polychef/LoginPageTest.java
+++ app/src/androidTest/java/ch/epfl/polychef/LoginPageTest.java
@@ -1,11 +1,10 @@
 package ch.epfl.polychef;
 
-import androidx.test.espresso.intent.rule.IntentsTestRule;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
-
 import static androidx.test.espresso.Espresso.onView;
+import androidx.test.espresso.intent.rule.IntentsTestRule;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import org.junit.Rule;
 import org.junit.Test;
