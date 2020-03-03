5538e1c39b9437ec0150fc5124cbe7be3131cc06
wilderino <thomas.wilde@epfl.ch>
2020-03-03 13:57:36 +0100

    putting back the good version

--- app/src/main/java/ch/epfl/polychef/GreetingActivity.java
+++ app/src/main/java/ch/epfl/polychef/GreetingActivity.java
@@ -2,19 +2,10 @@ package ch.epfl.polychef;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.TextView;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.FirebaseFirestore;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class GreetingActivity extends AppCompatActivity {
 
