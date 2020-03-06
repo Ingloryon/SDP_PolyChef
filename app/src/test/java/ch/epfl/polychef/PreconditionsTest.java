591bea97c7de6efd7be9e06c2c8712be65ac55a1
Guillaume Parchet <guillaume.parchet@epfl.ch>
2020-03-06 11:27:25 +0100

    Add Preconditions tests

--- app/src/test/java/ch/epfl/polychef/PreconditionsTest.java
+++ app/src/test/java/ch/epfl/polychef/PreconditionsTest.java
@@ -7,13 +7,16 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 public class PreconditionsTest {
 
     @Test
-    public void checkArgumentRejectsFalseBooleans(){
+    public void checkArgumentOnlyRejectsFalseBooleans(){
+        Preconditions.checkArgument(true, "The conditions is true");
+        Preconditions.checkArgument(true);
         assertThrows(IllegalArgumentException.class, () -> Preconditions.checkArgument(false));
         assertThrows(IllegalArgumentException.class, () -> Preconditions.checkArgument(false, "The conditions fails"));
     }
 
     @Test
-    public void checkIndexRejectsOutofBounds(){
+    public void checkIndexOnlyRejectsOutOfBounds(){
+        Preconditions.checkIndex(2, 5);
         assertThrows(IndexOutOfBoundsException.class, () -> Preconditions.checkIndex(14, 5));
         assertThrows(IndexOutOfBoundsException.class, () -> Preconditions.checkIndex(-5, 50));
     }
