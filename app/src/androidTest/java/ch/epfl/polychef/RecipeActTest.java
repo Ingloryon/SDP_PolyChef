1f28c8695f83740cd41e345512d31a90f75be0f2
Guillaume Parchet <guillaume.parchet@epfl.ch>
2020-03-08 14:49:50 +0100

    Fix Travis build by temporary adding foo tests in empty test classes

--- app/src/androidTest/java/ch/epfl/polychef/RecipeActTest.java
+++ app/src/androidTest/java/ch/epfl/polychef/RecipeActTest.java
@@ -1,22 +1,17 @@
 package ch.epfl.polychef;
 
-import androidx.test.espresso.intent.rule.IntentsTestRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
-import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static junit.framework.TestCase.assertTrue;
 
 @RunWith(AndroidJUnit4.class)
 public class RecipeActTest {
-    @Rule
-    public IntentsTestRule<MiniatureTestActivity> intentsTestRule = new IntentsTestRule<>(MiniatureTestActivity.class);
+    //@Rule
+    //public IntentsTestRule<MiniatureTestActivity> intentsTestRule = new IntentsTestRule<>(MiniatureTestActivity.class);
 
     @Test
     public void checkMiniatureTestActivityRunsCorrectly(){
-        onView(withId(R.id.cardList)).perform(click());
+        assertTrue(true);
     }
 }
--- app/src/androidTest/java/ch/epfl/polychef/RecipeActivityTest.java
+++ app/src/androidTest/java/ch/epfl/polychef/RecipeActivityTest.java
@@ -6,13 +6,19 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static junit.framework.TestCase.assertTrue;
+
 @RunWith(AndroidJUnit4.class)
 public class RecipeActivityTest {
-    //@Rule
-    //public IntentsTestRule<MiniatureTestActivity> intentsTestRule = new IntentsTestRule<>(MiniatureTestActivity.class);
+    @Rule
+    public IntentsTestRule<MiniatureTestActivity> intentsTestRule = new IntentsTestRule<>(MiniatureTestActivity.class);
 
     @Test
     public void checkMiniatureTestActivityRunsCorrectly(){
-
+        onView(withId(R.id.cardList)).perform(click());
+        assertTrue(true);
     }
 }
