811377e998c9231c7ea8f825b6e8196ae955bba8
Guillaume Parchet <guillaume.parchet@epfl.ch>
2020-03-08 14:33:26 +0100

    Create basic call of RecipeActivity functions for understanding

---
+++ app/src/androidTest/java/ch/epfl/polychef/RecipeActTest.java
@@ -0,0 +1,22 @@
+package ch.epfl.polychef;
+
+import androidx.test.espresso.intent.rule.IntentsTestRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+
+@RunWith(AndroidJUnit4.class)
+public class RecipeActTest {
+    @Rule
+    public IntentsTestRule<MiniatureTestActivity> intentsTestRule = new IntentsTestRule<>(MiniatureTestActivity.class);
+
+    @Test
+    public void checkMiniatureTestActivityRunsCorrectly(){
+        onView(withId(R.id.cardList)).perform(click());
+    }
+}
--- app/src/androidTest/java/ch/epfl/polychef/RecipeActivityTest.java
+++ app/src/androidTest/java/ch/epfl/polychef/RecipeActivityTest.java
@@ -1,6 +1,5 @@
 package ch.epfl.polychef;
 
-
 import androidx.test.espresso.intent.rule.IntentsTestRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import org.junit.Rule;
@@ -10,14 +9,10 @@ import org.junit.runner.RunWith;
 @RunWith(AndroidJUnit4.class)
 public class RecipeActivityTest {
     //@Rule
-    //public IntentsTestRule<RecipeActivity> intentsTestRule = new IntentsTestRule<>(RecipeActivity.class);
-
+    //public IntentsTestRule<MiniatureTestActivity> intentsTestRule = new IntentsTestRule<>(MiniatureTestActivity.class);
 
     @Test
     public void checkMiniatureTestActivityRunsCorrectly(){
-        new IntentsTestRule<>(MiniatureTestActivity.class);
+
     }
-
-
-
 }
