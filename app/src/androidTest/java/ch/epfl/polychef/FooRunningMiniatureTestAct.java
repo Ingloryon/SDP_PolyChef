4caad24576dd9924c10111ecce15105ee8545096
Guillaume Parchet <guillaume.parchet@epfl.ch>
2020-03-08 15:23:21 +0100

    Reorder imports of RecipeActTest in alphab order

--- app/src/androidTest/java/ch/epfl/polychef/FooRunningMiniatureTestAct.java
+++ app/src/androidTest/java/ch/epfl/polychef/FooRunningMiniatureTestAct.java
@@ -1,21 +1,19 @@
 package ch.epfl.polychef;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
 import androidx.test.espresso.intent.rule.IntentsTestRule;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import static junit.framework.TestCase.assertTrue;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.intent.Intents.intended;
-import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
-import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static junit.framework.TestCase.assertTrue;
 
 @RunWith(AndroidJUnit4.class)
 public class FooRunningMiniatureTestAct {
-    @Rule
-    public IntentsTestRule<MiniatureTestActivity> intentsTestRule = new IntentsTestRule<>(MiniatureTestActivity.class);
+    //@Rule
+    //public IntentsTestRule<MiniatureTestActivity> intentsTestRule = new IntentsTestRule<>(MiniatureTestActivity.class);
 
     @Test
     public void checkMiniatureTestActivityRunsCorrectly(){
