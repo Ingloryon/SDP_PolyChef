cd0720bf956cfcf5f93e93328bcb18f643a64b05
LucasSurmely <lucas.surmely@hotmail.com>
2020-03-09 17:57:05 +0100

    started adding miniatures to entry_page

---
+++ app/src/main/java/ch/epfl/polychef/recipe/OfflineRecipes.java
@@ -0,0 +1,162 @@
+package ch.epfl.polychef.recipe;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ch.epfl.polychef.R;
+
+public class OfflineRecipes {
+
+    public static List<Recipe> offlineRecipes = new ArrayList<>();
+
+    public static Recipe recipe1 = new RecipeBuilder()
+            .setName("")
+            .setRecipeDifficulty(Recipe.Difficulty.INTERMEDIATE)
+            .setEstimatedCookingTime(0)
+            .setPersonNumber(0)
+            .setEstimatedPreparationTime(0)
+            .addIngredient("", 0)
+            .addIngredient("", 0)
+            .addInstruction("")
+            .addInstruction("")
+            .addInstruction("")
+            .addInstruction("")
+            .addPicturePath(R.drawable.koreansteaktartare)
+            .build();
+
+    public static Recipe recipe2 = new RecipeBuilder()
+            .setName("")
+            .setRecipeDifficulty(Recipe.Difficulty.INTERMEDIATE)
+            .setEstimatedCookingTime(0)
+            .setPersonNumber(0)
+            .setEstimatedPreparationTime(0)
+            .addIngredient("", 0)
+            .addIngredient("", 0)
+            .addInstruction("")
+            .addInstruction("")
+            .addInstruction("")
+            .addInstruction("")
+            .addPicturePath(R.drawable.koreansteaktartare)
+            .build();
+
+    public static Recipe recipe3 = new RecipeBuilder()
+            .setName("")
+            .setRecipeDifficulty(Recipe.Difficulty.INTERMEDIATE)
+            .setEstimatedCookingTime(0)
+            .setPersonNumber(0)
+            .setEstimatedPreparationTime(0)
+            .addIngredient("", 0)
+            .addIngredient("", 0)
+            .addInstruction("")
+            .addInstruction("")
+            .addInstruction("")
+            .addInstruction("")
+            .addPicturePath(R.drawable.koreansteaktartare)
+            .build();
+
+    public static Recipe recipe4 = new RecipeBuilder()
+            .setName("")
+            .setRecipeDifficulty(Recipe.Difficulty.INTERMEDIATE)
+            .setEstimatedCookingTime(0)
+            .setPersonNumber(0)
+            .setEstimatedPreparationTime(0)
+            .addIngredient("", 0)
+            .addIngredient("", 0)
+            .addInstruction("")
+            .addInstruction("")
+            .addInstruction("")
+            .addInstruction("")
+            .addPicturePath(R.drawable.koreansteaktartare)
+            .build();
+
+    public static Recipe recipe5 = new RecipeBuilder()
+            .setName("")
+            .setRecipeDifficulty(Recipe.Difficulty.INTERMEDIATE)
+            .setEstimatedCookingTime(0)
+            .setPersonNumber(0)
+            .setEstimatedPreparationTime(0)
+            .addIngredient("", 0)
+            .addIngredient("", 0)
+            .addInstruction("")
+            .addInstruction("")
+            .addInstruction("")
+            .addInstruction("")
+            .addPicturePath(R.drawable.koreansteaktartare)
+            .build();
+
+    public static Recipe recipe6 = new RecipeBuilder()
+            .setName("")
+            .setRecipeDifficulty(Recipe.Difficulty.INTERMEDIATE)
+            .setEstimatedCookingTime(0)
+            .setPersonNumber(0)
+            .setEstimatedPreparationTime(0)
+            .addIngredient("", 0)
+            .addIngredient("", 0)
+            .addInstruction("")
+            .addInstruction("")
+            .addInstruction("")
+            .addInstruction("")
+            .addPicturePath(R.drawable.koreansteaktartare)
+            .build();
+
+    public static Recipe recipe7 = new RecipeBuilder()
+            .setName("")
+            .setRecipeDifficulty(Recipe.Difficulty.INTERMEDIATE)
+            .setEstimatedCookingTime(0)
+            .setPersonNumber(0)
+            .setEstimatedPreparationTime(0)
+            .addIngredient("", 0)
+            .addIngredient("", 0)
+            .addInstruction("")
+            .addInstruction("")
+            .addInstruction("")
+            .addInstruction("")
+            .addPicturePath(R.drawable.koreansteaktartare)
+            .build();
+
+    public static Recipe recipe8 = new RecipeBuilder()
+            .setName("")
+            .setRecipeDifficulty(Recipe.Difficulty.INTERMEDIATE)
+            .setEstimatedCookingTime(0)
+            .setPersonNumber(0)
+            .setEstimatedPreparationTime(0)
+            .addIngredient("", 0)
+            .addIngredient("", 0)
+            .addInstruction("")
+            .addInstruction("")
+            .addInstruction("")
+            .addInstruction("")
+            .addPicturePath(R.drawable.koreansteaktartare)
+            .build();
+
+    public static Recipe recipe9 = new RecipeBuilder()
+            .setName("")
+            .setRecipeDifficulty(Recipe.Difficulty.INTERMEDIATE)
+            .setEstimatedCookingTime(0)
+            .setPersonNumber(0)
+            .setEstimatedPreparationTime(0)
+            .addIngredient("", 0)
+            .addIngredient("", 0)
+            .addInstruction("")
+            .addInstruction("")
+            .addInstruction("")
+            .addInstruction("")
+            .addPicturePath(R.drawable.koreansteaktartare)
+            .build();
+
+    public static Recipe recipe10 = new RecipeBuilder()
+            .setName("")
+            .setRecipeDifficulty(Recipe.Difficulty.INTERMEDIATE)
+            .setEstimatedCookingTime(0)
+            .setPersonNumber(0)
+            .setEstimatedPreparationTime(0)
+            .addIngredient("", 0)
+            .addIngredient("", 0)
+            .addInstruction("")
+            .addInstruction("")
+            .addInstruction("")
+            .addInstruction("")
+            .addPicturePath(R.drawable.koreansteaktartare)
+            .build();
+    
+}
