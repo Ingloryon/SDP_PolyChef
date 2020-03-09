953d0ba753f85a2e4fb262f0b027f11f0411a919
ChapatteMateo <43453745+ChapatteMateo@users.noreply.github.com>
2020-03-09 09:04:09 +0100

    Update RealLoginPageTest.java

--- app/src/androidTest/java/ch/epfl/polychef/RealLoginPageTest.java
+++ app/src/androidTest/java/ch/epfl/polychef/RealLoginPageTest.java
@@ -3,11 +3,10 @@ package ch.epfl.polychef;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.ActivityTestRule;
 
+import static org.junit.Assert.assertNull;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import static org.junit.Assert.assertNull;
 
 @RunWith(AndroidJUnit4.class)
 public class RealLoginPageTest {
