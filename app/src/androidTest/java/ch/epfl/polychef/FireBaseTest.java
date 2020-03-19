bc892ee2a816805242ad2e117a3ce5549fc9e09f
ChapatteMateo <mateo.chapatte@epfl.ch>
2020-03-19 14:11:22 +0100

    added test for ReadRecipeFromFirebase

--- app/src/androidTest/java/ch/epfl/polychef/FireBaseTest.java
+++ app/src/androidTest/java/ch/epfl/polychef/FireBaseTest.java
@@ -2,23 +2,19 @@ package ch.epfl.polychef;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 import org.junit.Test;
 
 import ch.epfl.polychef.recipe.Recipe;
 
 import static ch.epfl.polychef.Firebase.addRecipeToFirebase;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class FireBaseTest {
 
-    public static FakeFireBase fakeFireBase=new FakeFireBase();
+    private FakeFireBase fakeFireBase=new FakeFireBase();
 
     @Test
     public void testThatAddingARecipeDoesNotThrowError(){
@@ -27,15 +23,16 @@ public class FireBaseTest {
 
     @Test(expected = IllegalArgumentException.class)
     public void testThatAddingANullRecipeThrowsError(){
-        fakeFireBase.addRecipeToFirebase(null);
+        addRecipeToFirebase(null);
     }
 
     @Test
     public void testThatReadRecipeFromFirebaseDoesNotThrowError(){
-        fakeFireBase.readRecipeFromFirebase(0,null);
+        FireHandler fireHandler=mock(FireHandler.class);
+        Firebase.readRecipeFromFirebase(0,fireHandler);
     }
 
-    private static class FakeFireBase extends Firebase {
+    public static class FakeFireBase extends Firebase {
         static {
             firebaseInstance = mock(FirebaseDatabase.class);
             DatabaseReference fakeDatabaseReference=mock(DatabaseReference.class);
