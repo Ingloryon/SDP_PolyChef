9a2e805f797d35c9607e534f491df966a0f9c4ad
Guillaume Parchet <guillaume.parchet@epfl.ch>
2020-03-19 14:16:44 +0100

    Add input parsing in PostRecipeFrag

--- app/src/main/java/ch/epfl/polychef/fragments/PostRecipeFragment.java
+++ app/src/main/java/ch/epfl/polychef/fragments/PostRecipeFragment.java
@@ -8,7 +8,9 @@ import android.widget.EditText;
 
 import androidx.fragment.app.Fragment;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import ch.epfl.polychef.R;
 import ch.epfl.polychef.recipe.Ingredient;
@@ -18,11 +20,12 @@ public class PostRecipeFragment extends Fragment {
     private String name;
     private List<String> recipeInstructions;
     private List<Ingredient> ingredients;
-
     private int personNumber;
     private int estimatedPreparationTime;
     private int estimatedCookingTime;
     private Recipe.Difficulty recipeDifficulty;
+
+    private Map<String, Boolean> wrongInputs = new HashMap<>();
 
     /**
      * Required empty public constructor.
@@ -42,18 +45,33 @@ public class PostRecipeFragment extends Fragment {
         EditText nameInput = getView().findViewById(R.id.nameInput);
         name = nameInput.getText().toString();
 
+        // TODO: inputs ingredients, instructions, difficulty
+
         EditText personNb = getView().findViewById(R.id.personNbInput);
-        personNumber = Integer.parseInt(personNb.getText().toString());
+        String persNb = personNb.getText().toString();
+
+        if (checkInputIsNumber(persNb)){
+            
+        } else {
+            personNumber = Integer.parseInt(personNb.getText().toString());
+        }
 
 
-        EditText editText = getView().findViewById(R.id.nameInput);
-        name = editText.getText().toString();
+        EditText prepTimeInput = getView().findViewById(R.id.prepTimeInput);
+        estimatedPreparationTime = Integer.parseInt(prepTimeInput.getText().toString());
+
+        EditText cookTimeInput = getView().findViewById(R.id.cookTimeInput);
+        estimatedCookingTime = Integer.parseInt(cookTimeInput.getText().toString());
 
     }
 
-    private boolean sanitizeNumbers(){
-        // TODO: Implement sanitization -> check entered number
-        return true;
+    private boolean checkInputIsNumber(String input){
+        return android.text.TextUtils.isDigitsOnly(input);
+    }
+
+    private void initializeWronginputsMap(){
+        wrongInputs.put("Title", false);
+        //TODO: rest of inputs
     }
 
 
