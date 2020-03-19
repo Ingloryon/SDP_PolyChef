75f50056be7448e88984ac3c0b1ae397b687a95c
Guillaume Parchet <guillaume.parchet@epfl.ch>
2020-03-19 13:26:32 +0100

    Start input collection for recipe creation

--- app/src/main/java/ch/epfl/polychef/fragments/PostRecipeFragment.java
+++ app/src/main/java/ch/epfl/polychef/fragments/PostRecipeFragment.java
@@ -4,12 +4,25 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import androidx.fragment.app.Fragment;
 
+import java.util.List;
+
 import ch.epfl.polychef.R;
+import ch.epfl.polychef.recipe.Ingredient;
+import ch.epfl.polychef.recipe.Recipe;
 
 public class PostRecipeFragment extends Fragment {
+    private String name;
+    private List<String> recipeInstructions;
+    private List<Ingredient> ingredients;
+
+    private int personNumber;
+    private int estimatedPreparationTime;
+    private int estimatedCookingTime;
+    private Recipe.Difficulty recipeDifficulty;
 
     /**
      * Required empty public constructor.
@@ -19,10 +32,28 @@ public class PostRecipeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_post_recipe, container, false);
 
+    }
 
+    private void getEnteredInputs(){
+        EditText nameInput = getView().findViewById(R.id.nameInput);
+        name = nameInput.getText().toString();
+
+        EditText personNb = getView().findViewById(R.id.personNbInput);
+        personNumber = Integer.parseInt(personNb.getText().toString());
+
+
+        EditText editText = getView().findViewById(R.id.nameInput);
+        name = editText.getText().toString();
+
+    }
+
+    private boolean sanitizeNumbers(){
+        // TODO: Implement sanitization -> check entered number
+        return true;
     }
 
 
