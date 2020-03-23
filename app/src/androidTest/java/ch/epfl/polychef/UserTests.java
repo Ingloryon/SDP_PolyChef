a173b87979e43e8af62aa4ac77fa1a9a8c9ec57a
Yanick Paulo-Amaro <yanick.pauloamaro@epfl.ch>
2020-03-23 14:19:05 +0100

    Add new tests for User

--- app/src/androidTest/java/ch/epfl/polychef/UserTests.java
+++ app/src/androidTest/java/ch/epfl/polychef/UserTests.java
@@ -68,8 +68,6 @@ public class UserTests {
 
         @Override
         public FirebaseDatabase getDatabase() {
-            //mockInit();
-            //newUserTest();
             return mockDatabase;
         }
 
@@ -150,7 +148,7 @@ public class UserTests {
     }
 
     @Test
-    public void testOldUser(){
+    public void oldUserTest(){
         DatabaseReference mockOldUserRef = mock(DatabaseReference.class);
         DataSnapshot mockSnapshotChild = mock(DataSnapshot.class);
 
@@ -197,6 +195,135 @@ public class UserTests {
 
             return null;
         });
+    }
+
+    @Test
+    public void gettersWorkOnNewUser() {
+
+        String email = mockUserEmail;
+        String username = mockUserName;
+        User alice = new User(email, username);
+
+        assertEquals(email, alice.getEmail());
+        assertEquals(username, alice.getUsername());
+        assertEquals(0, alice.getRecipes().size());
+        assertEquals(0, alice.getFavourites().size());
+        assertEquals(0, alice.getSubscribers().size());
+        assertEquals(0, alice.getSubscriptions().size());
+    }
+
+    @Test
+    public void gettersWorkOnEmptyUser() {
+        User noOne = new User();
+
+        assertNull(noOne.getEmail());
+        assertNull(noOne.getUsername());
+        assertEquals(0, noOne.getRecipes().size());
+        assertEquals(0, noOne.getFavourites().size());
+        assertEquals(0, noOne.getSubscribers().size());
+        assertEquals(0, noOne.getSubscriptions().size());
+    }
+
+    @Test
+    public void canAddFavouriteRecipes() {
+        String email = mockUserEmail;
+        String username = mockUserName;
+        User alice = new User(email, username);
+
+        String recipe1 = "Recipe_1";
+        String recipe2 = "Recipe_2";
+        String recipe3 = "Recipe_3";
+        String recipe4 = "Recipe_4";
+
+        alice.addFavourite(recipe1);
+        assertEquals(1, alice.getFavourites().size());
+        assertTrue(alice.getFavourites().contains(recipe1));
+
+        alice.addFavourite(recipe2);
+        assertEquals(2, alice.getFavourites().size());;
+        assertTrue(alice.getFavourites().contains(recipe1));
+        assertTrue(alice.getFavourites().contains(recipe2));
+
+        alice.addFavourite(recipe3);
+        assertEquals(3, alice.getFavourites().size());
+        assertTrue(alice.getFavourites().contains(recipe1));
+        assertTrue(alice.getFavourites().contains(recipe2));
+        assertTrue(alice.getFavourites().contains(recipe3));
+
+        alice.addFavourite(recipe4);
+        assertEquals(4, alice.getFavourites().size());
+        assertTrue(alice.getFavourites().contains(recipe1));
+        assertTrue(alice.getFavourites().contains(recipe2));
+        assertTrue(alice.getFavourites().contains(recipe3));
+        assertTrue(alice.getFavourites().contains(recipe4));
+    }
+
+    @Test
+    public void canAddSubscribers() {
+        String email = mockUserEmail;
+        String username = mockUserName;
+        User alice = new User(email, username);
+
+        String subscriber1 = "Subscriber_1";
+        String subscriber2 = "Subscriber_2";
+        String subscriber3 = "Subscriber_3";
+        String subscriber4 = "Subscriber_4";
+
+        alice.addSubscriber(subscriber1);
+        assertEquals(1, alice.getSubscribers().size());
+        assertTrue(alice.getSubscribers().contains(subscriber1));
+
+        alice.addSubscriber(subscriber2);
+        assertEquals(2, alice.getSubscribers().size());
+        assertTrue(alice.getSubscribers().contains(subscriber1));
+        assertTrue(alice.getSubscribers().contains(subscriber2));
+
+        alice.addSubscriber(subscriber3);
+        assertEquals(3, alice.getSubscribers().size());
+        assertTrue(alice.getSubscribers().contains(subscriber1));
+        assertTrue(alice.getSubscribers().contains(subscriber2));
+        assertTrue(alice.getSubscribers().contains(subscriber3));
+
+        alice.addSubscriber(subscriber4);
+        assertEquals(4, alice.getSubscribers().size());
+        assertTrue(alice.getSubscribers().contains(subscriber1));
+        assertTrue(alice.getSubscribers().contains(subscriber2));
+        assertTrue(alice.getSubscribers().contains(subscriber3));
+        assertTrue(alice.getSubscribers().contains(subscriber4));
+    }
+
+    @Test
+    public void canAddSubscriptions() {
+        String email = mockUserEmail;
+        String username = mockUserName;
+        User alice = new User(email, username);
+
+        String subscription1 = "Subscription_1";
+        String subscription2 = "Subscription_2";
+        String subscription3 = "Subscription_3";
+        String subscription4 = "Subscription_4";
+
+        alice.addSubscriptions(subscription1);
+        assertEquals(1, alice.getSubscriptions().size());
+        assertTrue(alice.getSubscriptions().contains(subscription1));
+
+        alice.addSubscriptions(subscription2);
+        assertEquals(2, alice.getSubscriptions().size());
+        assertTrue(alice.getSubscriptions().contains(subscription1));
+        assertTrue(alice.getSubscriptions().contains(subscription2));
+
+        alice.addSubscriptions(subscription3);
+        assertEquals(3, alice.getSubscriptions().size());
+        assertTrue(alice.getSubscriptions().contains(subscription1));
+        assertTrue(alice.getSubscriptions().contains(subscription2));
+        assertTrue(alice.getSubscriptions().contains(subscription3));
+
+        alice.addSubscriptions(subscription4);
+        assertEquals(4, alice.getSubscriptions().size());
+        assertTrue(alice.getSubscriptions().contains(subscription1));
+        assertTrue(alice.getSubscriptions().contains(subscription2));
+        assertTrue(alice.getSubscriptions().contains(subscription3));
+        assertTrue(alice.getSubscriptions().contains(subscription4));
     }
 }
 
