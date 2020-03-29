d3a54c7622e0a1cdd796f4870d6fc0453ee84b8a
wilderino <thomas.wilde@epfl.ch>
2020-03-29 23:12:18 +0200

    added the ui and the baic of the search

--- app/src/main/java/ch/epfl/polychef/fragments/OnlineMiniaturesFragment.java
+++ app/src/main/java/ch/epfl/polychef/fragments/OnlineMiniaturesFragment.java
@@ -1,15 +1,24 @@
 package ch.epfl.polychef.fragments;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,7 +32,10 @@ import ch.epfl.polychef.recipe.Recipe;
 
 public class OnlineMiniaturesFragment extends Fragment implements CallNotifier<Recipe> {
 
+    private static final String TAG = "OnlineMiniaturesFrag";
     private RecyclerView onlineRecyclerView;
+
+    private Button searchButton;
 
     private List<Recipe> dynamicRecipeList = new ArrayList<>();
 
@@ -72,6 +84,13 @@ public class OnlineMiniaturesFragment extends Fragment implements CallNotifier<R
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        searchButton = getView().findViewById(R.id.searchButton);
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setSearchButton(view);
+            }
+        });
         // For now when we enter the page we load the offline recipes first
         // add a certain number of recipes at this end of the actual list
         recipeStorage.getNRecipesOneByOne(nbOfRecipesLoadedAtATime, 1, this);
@@ -92,6 +111,57 @@ public class OnlineMiniaturesFragment extends Fragment implements CallNotifier<R
 
     public RecyclerView getRecyclerView(){
         return onlineRecyclerView;
+    }
+
+    private void setSearchButton(View view){
+        FirebaseDatabase database=recipeStorage.getFirebaseDatabase();
+        DatabaseReference idRef = database.getReference("id");
+        idRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value
+                int value = dataSnapshot.getValue(Integer.class);
+                searchForRecipe(value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
+
+    }
+
+    private void searchForRecipe(int id){
+        FirebaseDatabase database=recipeStorage.getFirebaseDatabase();
+        for(int i=1;i<=id;i++){
+            DatabaseReference nameRef = database.getReference("recipe").child(Integer.toString(i)).child("name");
+            nameRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    // This method is called once with the initial value
+                    String value = dataSnapshot.getValue(String.class);
+                    compareName(value);
+                }
+
+                @Override
+                public void onCancelled(DatabaseError error) {
+                    // Failed to read value
+                    Log.w(TAG, "Failed to read value.", error.toException());
+                }
+            });
+        }
+    }
+
+    private void compareName(String value){
+        String searchInput=((EditText)getView().findViewById(R.id.searchBar)).getText().toString();
+        searchInput=searchInput.toLowerCase();
+        value=value.toLowerCase();
+        if(searchInput.contains(value) || value.contains(searchInput)){
+            Log.w(TAG,value+" is a correct search");
+        }
+
     }
 
 }
