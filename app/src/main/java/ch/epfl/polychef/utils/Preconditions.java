3a570231fa43ddebc553b7bbb93bf5156a1bc97b
Guillaume Parchet <guillaume.parchet@epfl.ch>
2020-04-01 18:52:43 +0200

    Fix code smell on variable name in Precondition

--- app/src/main/java/ch/epfl/polychef/utils/Preconditions.java
+++ app/src/main/java/ch/epfl/polychef/utils/Preconditions.java
@@ -3,14 +3,14 @@ package ch.epfl.polychef.utils;
 public final class Preconditions {
     //private Preconditions() {}
 
-    public static void checkArgument(boolean b, String log) {
-        if (! b) {
+    public static void checkArgument(boolean bool, String log) {
+        if (! bool) {
             throw new IllegalArgumentException(log);
         }
     }
 
-    public static void checkArgument(boolean b) {
-        if (! b) {
+    public static void checkArgument(boolean bool) {
+        if (! bool) {
             throw new IllegalArgumentException();
         }
     }
