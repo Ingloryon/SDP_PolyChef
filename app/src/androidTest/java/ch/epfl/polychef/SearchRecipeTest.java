be40877f5c6db2e2721b9ec22de46d265b86ab41
ChapatteMateo <mateo.chapatte@epfl.ch>
2020-04-02 13:55:25 +0200

    built bases to test SearchRecipe

--- app/src/androidTest/java/ch/epfl/polychef/SearchRecipeTest.java
+++ app/src/androidTest/java/ch/epfl/polychef/SearchRecipeTest.java
@@ -1,4 +1,36 @@
 package ch.epfl.polychef;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import ch.epfl.polychef.recipe.SearchRecipe;
+
+import static org.mockito.Mockito.when;
+
 public class SearchRecipeTest {
+
+    SearchRecipe mockSearchRecipe;
+    FirebaseDatabase mockDataBase;
+    DatabaseReference mockDatabaseReference;
+
+    @Before
+    public void initTests(){
+        mockSearchRecipe= Mockito.mock(SearchRecipe.class,Mockito.CALLS_REAL_METHODS);
+        mockDataBase= Mockito.mock(FirebaseDatabase.class);
+        mockDatabaseReference=Mockito.mock(DatabaseReference.class);
+
+        when(mockSearchRecipe.getDatabase()).thenReturn(mockDataBase);
+
+        when(mockDataBase.getReference()).thenReturn(mockDatabaseReference);
+    }
+
+    @Test
+    public void testSearchForRecipe(){//String query, CallHandler<List<Recipe>> caller) {
+
+        //searchRecipe(query, this::compareName, caller);
+    }
 }
