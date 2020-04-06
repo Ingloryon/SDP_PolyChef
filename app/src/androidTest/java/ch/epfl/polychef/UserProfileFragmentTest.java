b7856ffb5af624781e5566b0bdc77214863c541a
LucasSurmely <lucas.surmely@hotmail.com>
2020-04-06 15:40:39 +0200

    continued test need to complete

--- app/src/androidTest/java/ch/epfl/polychef/UserProfileFragmentTest.java
+++ app/src/androidTest/java/ch/epfl/polychef/UserProfileFragmentTest.java
@@ -14,13 +14,18 @@ import org.junit.Test;
 import org.mockito.Mockito;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import ch.epfl.polychef.pages.HomePage;
+import ch.epfl.polychef.recipe.Ingredient;
+import ch.epfl.polychef.recipe.Recipe;
+import ch.epfl.polychef.recipe.RecipeBuilder;
 import ch.epfl.polychef.recipe.RecipeStorage;
 import ch.epfl.polychef.users.User;
 import ch.epfl.polychef.users.UserStorage;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
 
@@ -29,7 +34,19 @@ public class UserProfileFragmentTest {
     private String mockEmail = "mock@email.com";
     private String mockUsername = "mockUsername";
 
+    private RecipeStorage fakeStorage;
+
     private int numberOfRecipes = 5;
+
+    private Recipe testRecipe1 = new RecipeBuilder()
+            .setName("test1")
+            .setRecipeDifficulty(Recipe.Difficulty.EASY)
+            .addInstruction("test1instruction")
+            .setPersonNumber(4)
+            .setEstimatedCookingTime(30)
+            .setEstimatedPreparationTime(30)
+            .addIngredient("test1", 1.0, Ingredient.Unit.CUP)
+            .build();
 
     private SingleActivityFactory<HomePage> fakeHomePage = new SingleActivityFactory<HomePage>(
             HomePage.class) {
@@ -62,6 +79,8 @@ public class UserProfileFragmentTest {
 
     class FakeHomePage extends HomePage {
 
+        List<String> userRecipesUUID = new ArrayList<>();
+
         @Override
         public UserStorage getUserStorage(){
             UserStorage mockUserStorage = Mockito.mock(UserStorage.class);
@@ -69,15 +88,22 @@ public class UserProfileFragmentTest {
             when(mockUserStorage.getPolyChefUser()).thenReturn(mockUser);
             when(mockUser.getEmail()).thenReturn(mockEmail);
             when(mockUser.getUsername()).thenReturn(mockUsername);
-            ArrayList<String> recipes = new ArrayList<>(numberOfRecipes);
-            when(mockUser.getRecipes()).thenReturn(recipes);    //TODO choose mock recipes
+            when(mockUser.getRecipes()).thenReturn(userRecipesUUID);    //TODO choose mock recipes
             return mockUserStorage;
         }
 
         @Override
         public RecipeStorage getRecipeStorage(){
             RecipeStorage mockRecipeStorage = Mockito.mock(RecipeStorage.class);
-            doNothing().when(mockRecipeStorage).readRecipeFromUUID(any(String.class), any(CallHandler.class));
+            doAnswer(invocation -> {
+
+                String UUID = invocation.getArgument(0);
+                CallHandler<Recipe> caller = invocation.getArgument(1);
+
+                caller.onSuccess(testRecipe1);
+
+                return null;
+            }).when(mockRecipeStorage).readRecipeFromUUID(any(String.class), any(CallHandler.class));
             return mockRecipeStorage;
         }
 
@@ -86,7 +112,6 @@ public class UserProfileFragmentTest {
             FirebaseUser mockUser = Mockito.mock(FirebaseUser.class);
             return mockUser;
         }
-
 
     }
 }
