0b2549f448c7c5ab024f7b33c5e5f8094c841025
Yanick Paulo-Amaro <yanick.pauloamaro@epfl.ch>
2020-04-09 22:01:37 +0200

    Fix posting recipe with no pictures

--- app/src/main/java/ch/epfl/polychef/fragments/PostRecipeFragment.java
+++ app/src/main/java/ch/epfl/polychef/fragments/PostRecipeFragment.java
@@ -195,7 +195,7 @@ public class PostRecipeFragment extends Fragment {
             if(currentMiniature != null) {
                 imageHandler.uploadFromUri(currentMiniature, miniatureName, "TODO:USER", postedRecipe.getRecipeUuid());
             }
-            for(int i = 0; i <= currentMealPictures.size(); ++i) {
+            for(int i = 0; i < currentMealPictures.size(); ++i) {
                 imageHandler.uploadFromUri(currentMealPictures.get(i), postedRecipe.getPicturesPath().get(i), "TODO:USER", postedRecipe.getRecipeUuid());
             }
             hostActivity.getRecipeStorage().addRecipe(postedRecipe);
