ce45d87aa019a7a858165e058782f305e7304dfb
ChapatteMateo <mateo.chapatte@epfl.ch>
2020-04-23 11:42:11 +0200

    added the basic test for VoiceRecognizer to see if Travis is capable of runing it

--- app/src/androidTest/java/ch/epfl/polychef/utils/VoiceRecognizerTest.java
+++ app/src/androidTest/java/ch/epfl/polychef/utils/VoiceRecognizerTest.java
@@ -3,24 +3,39 @@ package ch.epfl.polychef.utils;
 
 import android.app.Activity;
 
+import androidx.test.espresso.intent.rule.IntentsTestRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 
-import ch.epfl.polychef.CallNotifier;
+import java.util.concurrent.TimeUnit;
 
+import ch.epfl.polychef.CallNotifier;
+import ch.epfl.polychef.pages.EntryPage;
+
+@RunWith(AndroidJUnit4.class)
 public class VoiceRecognizerTest {
+
+    @Rule
+    public IntentsTestRule<EntryPage> intentsTestRule = new IntentsTestRule<>(EntryPage.class);
+
 
     CallNotifier<String> mockCallNotifier= Mockito.mock(CallNotifier.class);
 
     Activity mockActivity= Mockito.mock(Activity.class);
 
     @Test
-    public void VoiceRecognizerStartAndStopWithoutErrorTest(){
+    public void voiceRecognizerStartAndStopWithoutErrorTest(){
         VoiceRecognizer vr=new VoiceRecognizer(mockCallNotifier);
-        vr.start(mockActivity);
+        vr.start(intentsTestRule.getActivity());
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         vr.onStop();
     }
-
-
-
 }
