daac36a83d9ea804ec4b0bc64cba7632fa9364a2
ChapatteMateo <mateo.chapatte@epfl.ch>
2020-04-30 20:34:40 +0200

    allow to either launch FakeEntryPage or FakeHomePage

--- app/src/androidTest/java/ch/epfl/polychef/fragments/RateRecipeFragmentsTest.java
+++ app/src/androidTest/java/ch/epfl/polychef/fragments/RateRecipeFragmentsTest.java
@@ -3,10 +3,12 @@ package ch.epfl.polychef.fragments;
 import android.content.Intent;
 
 import androidx.test.espresso.contrib.RecyclerViewActions;
+import androidx.test.espresso.matcher.RootMatchers;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.ActivityTestRule;
 import androidx.test.runner.intercepting.SingleActivityFactory;
 
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -17,7 +19,6 @@ import ch.epfl.polychef.pages.EntryPageTest;
 import ch.epfl.polychef.pages.HomePage;
 import ch.epfl.polychef.pages.HomePageTest;
 
-import static androidx.test.espresso.Espresso.onData;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
@@ -25,10 +26,10 @@ import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withSpinnerText;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
-import static net.bytebuddy.matcher.ElementMatchers.is;
 import static org.hamcrest.Matchers.containsString;
-import static org.hamcrest.Matchers.instanceOf;
-import static org.hamcrest.core.AllOf.allOf;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
+
 
 @RunWith(AndroidJUnit4.class)
 public class RateRecipeFragmentsTest {
@@ -42,19 +43,30 @@ public class RateRecipeFragmentsTest {
         }
     };
 
-    /*private SingleActivityFactory<HomePage> fakeHomePage = new SingleActivityFactory<HomePage>(
+    private SingleActivityFactory<HomePage> fakeHomePage = new SingleActivityFactory<HomePage>(
             HomePage.class) {
         @Override
         protected HomePage create(Intent intent) {
             HomePage activity = new HomePageTest.FakeHomePage();
             return activity;
         }
-    };*/
+    };
 
     @Rule
-    public ActivityTestRule<EntryPage> intentsTestRule = new ActivityTestRule<>(fakeEntryPage, false,
-            true);
+    public ActivityTestRule<EntryPage> intentsTestRule_Entry = new ActivityTestRule<>(fakeEntryPage, false,
+            false);
+    @Rule
+    public ActivityTestRule<HomePage> intentsTestRule_Home = new ActivityTestRule<>(fakeHomePage, false,
+            false);
 
+
+    private void launchFakeEntryPage() {
+        intentsTestRule_Entry.launchActivity(new Intent());
+    }
+
+    private void launchFakeHomePage() {
+        intentsTestRule_Home.launchActivity(new Intent());
+    }
 
     @Test
     public void createInstanceOfRateRecipeFragmentDoesNotThrowError(){
@@ -63,22 +75,29 @@ public class RateRecipeFragmentsTest {
 
     @Test
     public void rateButtonIsDisplayedAndDisplayCorrectText(){
+        launchFakeEntryPage();
         onView(withId(R.id.miniaturesOfflineList)).perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
         onView(withId(R.id.buttonRate)).check(matches(isDisplayed()));
         onView(withId(R.id.buttonRate)).check(matches(withText(R.string.RateButton)));
     }
 
-    /*public void rateSpinnerHasTheRightTexts() {
+    /*@Test
+    public void rateSpinnerHasTheRightTexts() {
+        onView(withId(R.id.miniaturesOfflineList)).perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
+        onView(withId(R.id.buttonRate)).perform(click());
         onView(withId(R.id.RateChoices)).perform(click());
-        onData(allOf(is(instanceOf(String.class)), is(selectionText))).perform(click());
-        onView(withId(R.id.RateChoices)).check(matches(withSpinnerText(containsString(selectionText))));
+        onView(withId(R.id.RateChoices)).check(matches(withSpinnerText(containsString("0"))));
     }*/
 
     @Test
     public void toastIsDisplayedIfTryToRateWhileNotLoggedIn(){
+        launchFakeEntryPage();
         onView(withId(R.id.miniaturesOfflineList)).perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
         onView(withId(R.id.buttonRate)).perform(click());
-
+        onView(withText(R.string.errorOnlineFeature))
+                .inRoot(RootMatchers.withDecorView(not(is(intentsTestRule_Entry.getActivity()
+                        .getWindow().getDecorView()))))
+                .check(matches(isDisplayed()));
     }
 
 
