16647add0ef676137c1c7547c0d305bf570a3293
wilderino <thomas.wilde@epfl.ch>
2020-05-06 13:41:16 +0200

    a bit of codeclimate

--- app/src/main/java/ch/epfl/polychef/utils/Similarity.java
+++ app/src/main/java/ch/epfl/polychef/utils/Similarity.java
@@ -2,14 +2,17 @@ package ch.epfl.polychef.utils;
 
 public class Similarity {
     public static double similarity(String s1, String s2) {
-        String longer = s1, shorter = s2;
+        String longer = s1;
+        String shorter = s2;
         if (s1.length() < s2.length()) {
-            longer = s2; shorter = s1;
+            longer = s2;
+            shorter = s1;
         }
         int longerLength = longer.length();
-        if (longerLength == 0) { return 1.0;}
+        if (longerLength == 0) {
+            return 1.0;
+        }
         return (longerLength - editDistance(longer, shorter)) / (double) longerLength;
-
     }
 
 
@@ -21,21 +24,22 @@ public class Similarity {
         for (int i = 0; i <= s1.length(); i++) {
             int lastValue = i;
             for (int j = 0; j <= s2.length(); j++) {
-                if (i == 0)
+                if (i == 0) {
                     costs[j] = j;
-                else {
+                } else {
                     if (j > 0) {
                         int newValue = costs[j - 1];
-                        if (s1.charAt(i - 1) != s2.charAt(j - 1))
-                            newValue = Math.min(Math.min(newValue, lastValue),
-                                    costs[j]) + 1;
+                        if (s1.charAt(i - 1) != s2.charAt(j - 1)){
+                            newValue = Math.min(Math.min(newValue, lastValue), costs[j]) + 1;
+                        }
                         costs[j - 1] = lastValue;
                         lastValue = newValue;
                     }
                 }
             }
-            if (i > 0)
+            if (i > 0) {
                 costs[s2.length()] = lastValue;
+            }
         }
         return costs[s2.length()];
     }
