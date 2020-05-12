380532ae8dc1e8cc77ce179a72ea5eb76fd70a6c
Guillaume Parchet <guillaume.parchet@epfl.ch>
2020-05-12 11:51:23 +0200

    Add Cuistot achievement

--- app/src/main/java/ch/epfl/polychef/users/gamification/AchievementsList.java
+++ app/src/main/java/ch/epfl/polychef/users/gamification/AchievementsList.java
@@ -1,12 +1,14 @@
 package ch.epfl.polychef.users.gamification;
 
 import java.util.ArrayList;
+import java.util.function.Function;
+
+import ch.epfl.polychef.users.User;
 
 public final class AchievementsList {
     private static final int STANDART_NB_LEVELS = 3;
 
     public static final Achievement Cuisto_Achievement = createCuistotAchievement();
-
 
 
     private static Achievement createCuistotAchievement(){
@@ -17,15 +19,18 @@ public final class AchievementsList {
         picturesPaths.add("cuisto_diamond");
 
         ArrayList<String> picturesLabels = new ArrayList<>();
-        picturesPaths.add("cuisto_bronze");
-        picturesPaths.add("cuisto_silver");
-        picturesPaths.add("cuisto_diamond");
+        picturesLabels.add("Starting Chef");
+        picturesLabels.add("Casual Chef");
+        picturesLabels.add("Legacy Chef");
 
+        ArrayList<Integer> levelSteps = new ArrayList<>();
+        levelSteps.add(1);
+        levelSteps.add(10);
+        levelSteps.add(50);
 
+        Function<User, Integer> getUserNbRecipes = u -> u.getRecipes().size();
 
-
-
-        return null; //new Achievement("Cuistot", STANDART_NB_LEVELS, picturesPaths, );
+        return new Achievement("Cuistot", STANDART_NB_LEVELS, picturesPaths, picturesLabels, levelSteps, getUserNbRecipes);
     }
 
     //"Followed"
