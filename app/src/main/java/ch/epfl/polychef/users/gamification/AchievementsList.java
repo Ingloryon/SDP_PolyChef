022c7d98f748dcfeb4b63146f0deba9b13a92b0c
Guillaume Parchet <guillaume.parchet@epfl.ch>
2020-05-12 11:52:19 +0200

    Renames cuisto to cuistot in pictures names

--- app/src/main/java/ch/epfl/polychef/users/gamification/AchievementsList.java
+++ app/src/main/java/ch/epfl/polychef/users/gamification/AchievementsList.java
@@ -8,15 +8,15 @@ import ch.epfl.polychef.users.User;
 public final class AchievementsList {
     private static final int STANDART_NB_LEVELS = 3;
 
-    public static final Achievement Cuisto_Achievement = createCuistotAchievement();
+    public static final Achievement Cuistot_Achievement = createCuistotAchievement();
 
 
     private static Achievement createCuistotAchievement(){
 
         ArrayList<String> picturesPaths = new ArrayList<>();
-        picturesPaths.add("cuisto_bronze");
-        picturesPaths.add("cuisto_silver");
-        picturesPaths.add("cuisto_diamond");
+        picturesPaths.add("cuistot_bronze");
+        picturesPaths.add("cuistot_silver");
+        picturesPaths.add("cuistot_diamond");
 
         ArrayList<String> picturesLabels = new ArrayList<>();
         picturesLabels.add("Starting Chef");
