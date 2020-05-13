329350a83606d80a77eb99ca915ad663896f7b41
Yanick Paulo-Amaro <yanick.pauloamaro@epfl.ch>
2020-05-13 14:04:00 +0200

    Add progress bar during auto login

--- app/src/main/java/ch/epfl/polychef/pages/EntryPage.java
+++ app/src/main/java/ch/epfl/polychef/pages/EntryPage.java
@@ -5,7 +5,9 @@ import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -60,9 +62,23 @@ public class EntryPage extends AppCompatActivity implements CallHandler<User> {
 
     protected void goHomeIfConnected() {
         if(FirebaseAuth.getInstance().getCurrentUser() != null) {
-//            startActivity(new Intent(this, HomePage.class));
+            findViewById(R.id.autoLoginBackground).setVisibility(View.VISIBLE);
+            findViewById(R.id.autoLoginProgress).setVisibility(View.VISIBLE);
+
+            preventInteractions();
+
+            Toast.makeText(this, "Auto connect" , Toast.LENGTH_SHORT).show();
             UserStorage.getInstance().initializeUserFromAuthenticatedUser(this);
         }
+    }
+
+    private void preventInteractions(){
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE,
+                WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
+    }
+
+    private void allowInteractions(){
+        getWindow().clearFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
     }
 
     /** Called when the user taps the log button. */
@@ -77,7 +93,17 @@ public class EntryPage extends AppCompatActivity implements CallHandler<User> {
     }
 
     @Override
-    public void onSuccess(User data) {
+    public synchronized void onSuccess(User data) {
+        try{
+            wait(500);
+        } catch(InterruptedException e){
+
+        }
+
+        allowInteractions();
+
+        findViewById(R.id.autoLoginProgress).setVisibility(View.GONE);
+        findViewById(R.id.autoLoginBackground).setVisibility(View.GONE);
         startActivity(new Intent(this, HomePage.class));
     }
 }
