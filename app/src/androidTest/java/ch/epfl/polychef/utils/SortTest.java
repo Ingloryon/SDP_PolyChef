d152dda74a2e80e3b8c1bdb87a86593005acf425
wilderino <thomas.wilde@epfl.ch>
2020-05-20 19:13:32 +0200

    added tests

--- app/src/androidTest/java/ch/epfl/polychef/utils/SortTest.java
+++ app/src/androidTest/java/ch/epfl/polychef/utils/SortTest.java
@@ -22,10 +22,13 @@ public class SortTest {
         user2.getRating().addOpinion("1",5);
         User user3 = new User();
         user3.getRating().addOpinion("2",3);
+        User user4 = new User();
+        user4.getRating().addOpinion("0",1);
         List<Miniatures> list = new ArrayList<>();
         list.add(user1);
         list.add(user2);
         list.add(user3);
+        list.add(user4);
         Sort.sortByRate(list);
         assertEquals(user2, list.get(0));
     }
@@ -43,11 +46,36 @@ public class SortTest {
         Recipe recipe1 = rb.addIngredient("Carrots", 300d, Ingredient.Unit.NONE).build();
         Recipe recipe2 = rb.addIngredient("Chocolate", 300d, Ingredient.Unit.NONE).build();
         Recipe recipe3 = rb.addIngredient("Rat", 300d, Ingredient.Unit.NONE).build();
+        Recipe recipe4 = rb.addIngredient("Rat", 300d, Ingredient.Unit.NONE).build();
         List<Miniatures> list = new ArrayList<>();
         list.add(recipe1);
         list.add(recipe2);
         list.add(recipe3);
+        list.add(recipe4);
         Sort.sortByIngredientSimilarity(list,"Chocolate");
+        assertEquals(recipe2, list.get(0));
+    }
+
+    @Test
+    public void sortBySimilarity(){
+        RecipeBuilder rb = new RecipeBuilder();
+        rb.setEstimatedPreparationTime(4);
+        rb.setEstimatedCookingTime(5);
+        rb.setRecipeDifficulty(Recipe.Difficulty.HARD);
+        rb.setAuthor("testUser@polychef.ch");
+        rb.addInstruction("instruction");
+        rb.addIngredient("Carrots", 300d, Ingredient.Unit.NONE);
+        rb.setPersonNumber(1);
+        Recipe recipe1 = rb.setName("Chicken fried").build();
+        Recipe recipe2 = rb.setName("Chicken").build();
+        Recipe recipe3 = rb.setName("Carrot").build();
+        Recipe recipe4 = rb.setName("Carrot").build();
+        List<Miniatures> list = new ArrayList<>();
+        list.add(recipe1);
+        list.add(recipe2);
+        list.add(recipe3);
+        list.add(recipe4);
+        Sort.sortBySimilarity(list,"Chicken");
         assertEquals(recipe2, list.get(0));
     }
 }
