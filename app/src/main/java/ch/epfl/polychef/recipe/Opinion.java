bac20fe1b58037da1c8f8b48081786f6891aed83
Guillaume Parchet <guillaume.parchet@epfl.ch>
2020-05-22 17:11:26 +0200

    Add javadoc to Opinion

--- app/src/main/java/ch/epfl/polychef/recipe/Opinion.java
+++ app/src/main/java/ch/epfl/polychef/recipe/Opinion.java
@@ -5,6 +5,9 @@ import androidx.annotation.Nullable;
 import java.io.Serializable;
 import java.util.Objects;
 
+/**
+ * A class representing an opinion of a recipe (composed of a rate and a comment).
+ */
 public class Opinion implements Serializable {
 
     private String comment;
@@ -16,19 +19,36 @@ public class Opinion implements Serializable {
     public Opinion() {
     }
 
+    /**
+     * Constructs an opinion with no comment.
+     * @param rate the rate given to the recipe
+     */
     public Opinion(int rate) {
         this(rate, null);
     }
 
+    /**
+     * Constructs an opinion with a rate and a comment.
+     * @param rate the rate given to the recipe
+     * @param comment the comment concerning the recipe
+     */
     public Opinion(int rate, String comment) {
         this.rate = rate;
         this.comment = comment;
     }
 
+    /**
+     * Gets the rate of the Opinion.
+     * @return the rate of the Opinion
+     */
     public int getRate() {
         return rate;
     }
 
+    /**
+     * Gets the comment of the Opinion.
+     * @return the comment of the Opinion
+     */
     public String getComment() {
         return comment;
     }
