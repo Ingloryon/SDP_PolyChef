d0a97e0bc542392ee949c45ab87eb183387802fa
Guillaume Parchet <guillaume.parchet@epfl.ch>
2020-05-25 15:39:55 +0200

    Add javadoc, refactor methods of VoiceRecognizer

--- app/src/main/java/ch/epfl/polychef/utils/VoiceRecognizer.java
+++ app/src/main/java/ch/epfl/polychef/utils/VoiceRecognizer.java
@@ -19,19 +19,24 @@ public class VoiceRecognizer implements RecognitionListener {
 
     private static final String KWS_SEARCH = "wakeup";//keyword to start recognition
     private static final String MENU_SEARCH = "menu";//name of the recognition set of words
-    // Keyword we are looking for to activate recognition
-    private static final String KEYPHRASE = "poly chef";
+    private static final String KEYPHRASE = "poly chef"; // Keyword we are looking for to activate recognition
 
     //Recognition object
     private SpeechRecognizer recognizer;
-
     private CallNotifier<String> callNotifier;
 
+    /**
+     * Constructor of VoiceRecognizer.
+     * @param callNotifier the call notifier to associate it with
+     */
     public VoiceRecognizer(CallNotifier<String> callNotifier){
         this.callNotifier=callNotifier;
     }
 
-
+    /**
+     * Starts the voice recognizer.
+     * @param activity the activity where it starts
+     */
     @SuppressLint("StaticFieldLeak")
     public void start(Activity activity){
         // Recognizer initialization is a time-consuming and it involves IO,
@@ -59,8 +64,84 @@ public class VoiceRecognizer implements RecognitionListener {
         }.execute();
     }
 
+    /**
+     * Shuts down the voice recognizer.
+     */
+    public void onStop() {
+        if (recognizer != null) {
+            recognizer.cancel();
+            recognizer.shutdown();
+        }
+    }
+
+    /**
+     * Gets the speech recognizer.
+     * @return the speech recognizer
+     */
+    public SpeechRecognizer getRecognizer(){
+        return recognizer;
+    }
+
+    /**
+     * Sets a new recognizer for the voice recognizer.
+     * @param recognizer the new recognizer
+     */
     protected void setRecognizer(SpeechRecognizer recognizer){
         this.recognizer=recognizer;
+    }
+
+    /**
+     * Switches the search depending on the keywords.
+     * @param searchName the search name given by user
+     */
+    protected void switchSearch(String searchName) {
+        recognizer.stop();
+
+        if (searchName.equals(KWS_SEARCH)) {
+            recognizer.startListening(KWS_SEARCH);
+        }else {
+            recognizer.startListening(searchName, 10000);
+        }
+    }
+
+    @Override
+    public void onPartialResult(Hypothesis hypothesis) {
+        if (hypothesis != null) {
+            String text = hypothesis.getHypstr();
+
+            if (text.equals(KEYPHRASE)) {
+                switchSearch(MENU_SEARCH);
+            }
+        }
+    }
+
+    @Override
+    public void onResult(Hypothesis hypothesis) {
+        if (hypothesis != null && !hypothesis.getHypstr().equals(KEYPHRASE)) {
+            callNotifier.notify(hypothesis.getHypstr());
+        }
+    }
+
+    @Override
+    public void onBeginningOfSpeech() {
+        // there is nothing to do on the beginning of speech
+    }
+
+    @Override
+    public void onEndOfSpeech() {
+        if (!recognizer.getSearchName().equals(KWS_SEARCH)) {
+            switchSearch(KWS_SEARCH);
+        }
+    }
+
+    @Override
+    public void onError(Exception error) {
+        onStop();
+    }
+
+    @Override
+    public void onTimeout() {
+        switchSearch(KWS_SEARCH);
     }
 
     private void setupRecognizer(File assetsDir) throws IOException {
@@ -75,66 +156,4 @@ public class VoiceRecognizer implements RecognitionListener {
         File menuGrammar = new File(assetsDir, "mymenu.gram");
         recognizer.addGrammarSearch(MENU_SEARCH, menuGrammar);
     }
-
-    public void onStop() {
-        if (recognizer != null) {
-            recognizer.cancel();
-            recognizer.shutdown();
-        }
-    }
-
-    @Override
-    public void onPartialResult(Hypothesis hypothesis) {
-        if (hypothesis == null) {
-            return;
-        }
-
-        String text = hypothesis.getHypstr();
-
-        if (text.equals(KEYPHRASE)) {
-            switchSearch(MENU_SEARCH);
-        }
-    }
-
-    @Override
-    public void onResult(Hypothesis hypothesis) {
-        if (hypothesis != null && !hypothesis.getHypstr().equals(KEYPHRASE)) {
-            callNotifier.notify(hypothesis.getHypstr());
-        }
-    }
-
-    @Override
-    public void onBeginningOfSpeech() {
-    }
-
-    @Override
-    public void onEndOfSpeech() {
-        if (!recognizer.getSearchName().equals(KWS_SEARCH)) {
-            switchSearch(KWS_SEARCH);
-        }
-    }
-
-    protected void switchSearch(String searchName) {
-        recognizer.stop();
-
-        if (searchName.equals(KWS_SEARCH)) {
-            recognizer.startListening(KWS_SEARCH);
-        }else {
-            recognizer.startListening(searchName, 10000);
-        }
-    }
-
-    @Override
-    public void onError(Exception error) {
-    }
-
-    @Override
-    public void onTimeout() {
-        switchSearch(KWS_SEARCH);
-    }
-
-    public SpeechRecognizer getRecognizer(){
-        return recognizer;
-    }
-
 }
