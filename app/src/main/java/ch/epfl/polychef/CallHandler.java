7914b46f1ce082f48ecc7426286221c98c7bbeb3
Guillaume Parchet <guillaume.parchet@epfl.ch>
2020-05-25 15:49:09 +0200

    Add javadoc in CallHandler

--- app/src/main/java/ch/epfl/polychef/CallHandler.java
+++ app/src/main/java/ch/epfl/polychef/CallHandler.java
@@ -1,8 +1,20 @@
 package ch.epfl.polychef;
 
+/**
+ * The interface of a CallHandler, can either success or fail its call.
+ * @param <T> the result type
+ */
+@SuppressWarnings("UnnecessaryInterfaceModifier")
 public interface CallHandler<T> {
 
+    /**
+     * What happens if the callHandler successes.
+     * @param data the required data
+     */
     public void onSuccess(T data);
 
+    /**
+     * What happens if the callHandler fails.
+     */
     public void onFailure();
 }
