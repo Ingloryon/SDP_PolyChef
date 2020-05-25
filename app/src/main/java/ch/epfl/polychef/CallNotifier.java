1eb56f283ea4678e12d127cfbebae7f7a86c3f53
Guillaume Parchet <guillaume.parchet@epfl.ch>
2020-05-25 15:53:54 +0200

    Add javadoc in CallNotifier

--- app/src/main/java/ch/epfl/polychef/CallNotifier.java
+++ app/src/main/java/ch/epfl/polychef/CallNotifier.java
@@ -1,8 +1,20 @@
 package ch.epfl.polychef;
 
+/**
+ * The interface of a CallNotifier, will notify or fail.
+ * @param <T> the result type
+ */
+@SuppressWarnings("UnnecessaryInterfaceModifier")
 public interface CallNotifier<T> {
 
+    /**
+     * What happens if manages to notify.
+     * @param data the required data
+     */
     public void notify(T data);
 
+    /**
+     * What happens if the CallNotifier fails.
+     */
     public void onFailure();
 }
